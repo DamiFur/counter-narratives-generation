35fa14d368e41ed6703721486d1fbb787c794cdd
DamiFur <damian.a.furman@gmail.com>
2023-11-19 16:23:02 -0300

    tweet for evaluating output of generators

--- tweet_cn_pair_extractor.py
+++ tweet_cn_pair_extractor.py
@@ -127,7 +127,7 @@ for f in tw_cn_pairs:
         preds = predictions[f][only_tw_text]
         w.write("{}\t{}\t{}\n".format(only_tw_text.replace("\t"," "), cn.replace("\t"," "), preds))
     w.write("===========================================\n")
-    w.write("\t".join(predictions[f]['categories']))
+    w.write("\t".join(str(predictions[f]['categories'])))
     total_avg = 0.0
     for idx, cat in enumerate(predictions[f]['categories']):
         if idx == 1:
