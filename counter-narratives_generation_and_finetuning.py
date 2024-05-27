effc4a9216836f517bb33bf77103708da574f3eb
DamiFur <damian.a.furman@gmail.com>
2024-05-27 11:49:24 -0300

    fix for only one type of cns

--- counter-narratives_generation_and_finetuning.py
+++ counter-narratives_generation_and_finetuning.py
@@ -537,7 +537,7 @@ def preprocess(sample, padding="max_length"):
         else:
             model_inputs = tokenizer.apply_chat_template(inputs, padding=padding, max_length=MAX_LENGTH, truncation=True, return_tensors="pt")
             model_inputs = model_inputs.to(device)
-        model_inputs["counterSpeech"] = sample["counterSpeech"]
+        model_inputs["labels"] = tokenizer(sample["counterSpeech"])["input_ids"]
     return model_inputs
 
 class StoppingCriteriaSub(StoppingCriteria):
